a1117089e385f921bab7e8ed6c8cf37c3e1e57c1
Jelte Liekens <jelte@wieni.be>
2017-01-10 16:23:26 +0100

    Add twig filter and fix implementation

--- src/TwigExtension/ImgixExtension.php
+++ src/TwigExtension/ImgixExtension.php
@@ -3,6 +3,7 @@
 namespace Drupal\imgix\TwigExtension;
 
 use Drupal\imgix\ImgixManagerInterface;
+use Drupal\imgix\Plugin\Field\FieldType\ImgixFieldType;
 
 class ImgixExtension extends \Twig_Extension
 {
@@ -12,7 +13,17 @@ class ImgixExtension extends \Twig_Extension
     {
         $this->imgixManager = $imgixManager;
     }
-
+    
+    public function getFilters()
+    {
+        return [
+            'imgix' => new \Twig_SimpleFilter(
+                'imgix',
+                [$this, 'imgix']
+            )
+        ];
+    }
+    
     public function getFunctions()
     {
         return [
@@ -33,9 +44,26 @@ class ImgixExtension extends \Twig_Extension
         return "imgix.twig_extension";
     }
     
-    public function imgix($url, $profile)
+    public function imgix($file, $presetSetting)
     {
-        return $this->imgixManager->renderProfile($url, $profile);
+        if ($file instanceof ImgixFieldType) {
+            $file = $file->getFile();
+        }
+        
+        $presets = $this->imgixManager->getPresets();
+    
+        $params = [];
+        if (isset($presets[$presetSetting])) {
+            $tmp = explode('&', $presets[$presetSetting]['query']);
+            foreach ($tmp as $value) {
+                $tmp2 = explode('=', $value);
+                $params[$tmp2[0]] = $tmp2[1];
+            }
+    
+            return $this->imgixManager->getImgixUrl($file, $params);
+        }
+        
+        return 'No valid preset found';
     }
     
 }
