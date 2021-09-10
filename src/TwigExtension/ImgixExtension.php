b3157652db77b823a611488a8c8e7eddc58326b8
Dieter Holvoet <dieter.holvoet@gmail.com>
2021-09-10 09:39:20 +0200

    Add support for passing entity reference field items to the Twig extension

--- src/TwigExtension/ImgixExtension.php
+++ src/TwigExtension/ImgixExtension.php
@@ -2,10 +2,11 @@
 
 namespace Drupal\imgix\TwigExtension;
 
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
 use Drupal\file\FileInterface;
 use Drupal\image\Plugin\Field\FieldType\ImageItem;
 use Drupal\imgix\ImgixManagerInterface;
-use Drupal\imgix\Plugin\Field\FieldType\DoNotUseImgixFieldType;
+use Drupal\media\MediaInterface;
 
 class ImgixExtension extends \Twig_Extension
 {
@@ -38,8 +39,17 @@ class ImgixExtension extends \Twig_Extension
 
     public function imgix($file, $preset)
     {
-        if ($file instanceof DoNotUseImgixFieldType) {
-            $file = $file->getFile();
+        if ($file instanceof EntityReferenceItem) {
+            $file = $file->get('entity')->getValue();
+        }
+
+        if ($file instanceof MediaInterface) {
+            $source = $file->getSource();
+            $field = $source->getConfiguration()['source_field'] ?? null;
+
+            if ($file->get($field)->entity) {
+                $file = $file->get($field)->entity;
+            }
         }
 
         if ($file instanceof ImageItem) {
